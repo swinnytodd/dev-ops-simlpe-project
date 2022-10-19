f6b2d17a8e670dc5a6c1cd3f4aea4a7029a8443e
Amantur Dzhamankulov <swinnytodd225@gmail.com>
2022-10-19 17:24:12 +0300

    DevOps simple project

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+resource "aws_instance" "ec2_example" {
+
+    count = 3
+    ami = "ami-096800910c1b781ba"  
+    instance_type = "t2.micro" 
+    key_name= "aws_key"
+    vpc_security_group_ids = [aws_security_group.my_webserver.id]
+    
+    tags = {
+        Name = "jenkins_test"
+    }
+
+}
+
+resource "aws_security_group" "my_webserver" {
+  name   = "My Security Group"
+  #vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "22", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "My SecurityGroup"
+  }
+}
